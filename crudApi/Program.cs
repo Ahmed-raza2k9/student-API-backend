81fe9b7e15f7bc2c3ec60e0f3f262fec1a7991d5
Ahmed raza <ahmedrazaj2k9@gmail.com>
2026-06-24 23:52:21 +0500

    removed open api from program.cs

--- crudApi/Program.cs
+++ crudApi/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
 builder.Services.AddDbContext<MyDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddCors(options =>
@@ -20,12 +20,6 @@ builder.Services.AddCors(options =>
     });
 });
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");

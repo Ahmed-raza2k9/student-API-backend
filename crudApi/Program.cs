f60f92dc65f70f8696c93255748c7fa0f06f9a8b
Ahmed raza <ahmedrazaj2k9@gmail.com>
2026-06-25 00:07:18 +0500

    in db memory version just for testing

--- crudApi/Program.cs
+++ crudApi/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 builder.Services.AddDbContext<MyDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseInMemoryDatabase("StudentDb"));
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
